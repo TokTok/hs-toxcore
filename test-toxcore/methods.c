b688b4c173097776db20f8d1b6bdc3f6e6fd8565
iphydf <iphydf@users.noreply.github.com>
2016-07-02 03:09:13 +0200

    Add response stub for KeyPair.newNonce

--- test-toxcore/methods.c
+++ test-toxcore/methods.c
@@ -1,5 +1,8 @@
 #include "methods.h"
 
+#define MIN(a,b) ((a) < (b) ? a : b)
+
+
 void
 call_method (msgpack_zone *zone, msgpack_object const *req, msgpack_object *res)
 {
@@ -8,4 +11,13 @@ call_method (msgpack_zone *zone, msgpack_object const *req, msgpack_object *res)
   printf ("\n");
 
   *res = *req;
+  res->via.array.ptr[0].via.u64 = 1; // response
+
+  msgpack_object_str name = req->via.array.ptr[2].via.str;
+#define NAME_IS(NAME) (memcmp (name.ptr, NAME, MIN (name.size, sizeof NAME)) == 0)
+  if (NAME_IS ("KeyPair.newNonce"))
+    {
+      printf ("Creating new nonce\n");
+      // TODO: create nonce and put it into *res.
+    }
 }
