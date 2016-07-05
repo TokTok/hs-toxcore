c9220409788769d0c45c8056ea0933264eab227b
iphydf <iphydf@users.noreply.github.com>
2016-07-05 13:42:47 +0100

    Fix infinite loop

--- test-toxcore/TestToxcore.hs
+++ test-toxcore/TestToxcore.hs
@@ -13,18 +13,18 @@ foreign import ccall "test_main" c_test_main :: Bool -> Word16 -> IO Word32
 
 
 data Result = Result
-  { line   :: Int
-  , result :: Int
-  , errno  :: Int
+  { line  :: Int
+  , code  :: Int
+  , errno :: Int
   }
 
 testMain :: Bool -> Int -> IO Result
 testMain collectSamples port = do
   result <- c_test_main collectSamples (fromIntegral port)
-  let line   = fromIntegral $ shift result (-16)
-  let errno  = fromIntegral $ shift result (- 8) .&. 0xff
-  let result = fromIntegral $       result       .&. 0xff
-  return $ Result line result errno
+  let line  = fromIntegral $ shift result (-16)
+  let errno = fromIntegral $ shift result (- 8) .&. 0xff
+  let code  = fromIntegral $       result       .&. 0xff
+  return $ Result line code errno
 
 
 errorDesc :: Int -> String
@@ -43,6 +43,6 @@ errorDesc = \case
 
 main :: IO ()
 main = do
-  Result { line, result, errno } <- testMain True 1234
-  when (result /= E_OK) $
-    putStrLn $ errorDesc result ++ ", errno=" ++ show errno ++ ", line=" ++ show line
+  Result { line, code, errno } <- testMain True 1234
+  when (code /= E_OK) $
+    putStrLn $ errorDesc code ++ ", errno=" ++ show errno ++ ", line=" ++ show line
