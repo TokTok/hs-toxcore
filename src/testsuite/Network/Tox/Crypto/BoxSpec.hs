b09801f30f173e74ba2c2b88dfb91e508e8f8f96
iphydf <iphydf@users.noreply.github.com>
2016-07-07 14:48:25 +0100

    Add more crypto box tests.
    There is a bug in the C implementation of newKeyPair, which is not exhibited by
    any current tests. Once CombinedKey.precompute is implemented, the newly added
    tests will fail, so we can then fix newKeyPair.

--- src/testsuite/Network/Tox/Crypto/BoxSpec.hs
+++ src/testsuite/Network/Tox/Crypto/BoxSpec.hs
@@ -2,18 +2,50 @@
 {-# LANGUAGE Trustworthy       #-}
 module Network.Tox.Crypto.BoxSpec where
 
-import           Control.Monad.IO.Class (liftIO)
-import           Network.Tox.RPCTest    (runTest)
+import           Control.Monad.IO.Class         (liftIO)
+import           Network.Tox.RPCTest            (runTest)
 import           Test.Hspec
 import           Test.QuickCheck
 
-import qualified Network.Tox.Crypto.Box as Box
+import qualified Network.Tox.Crypto.Box         as Box
+import qualified Network.Tox.Crypto.CombinedKey as CombinedKey
+import           Network.Tox.Crypto.KeyPair     (KeyPair (..))
+import qualified Network.Tox.Crypto.KeyPair     as KeyPair
 
 
 spec :: Spec
-spec =
-  it "should decrypt encrypted data" $
+spec = do
+  it "should decrypt its own encrypted data" $
     property $ \combinedKey nonce plainText -> runTest $ do
       cipherText <- Box.encryptC combinedKey nonce plainText
-      Just decryptedText <- Box.decryptC combinedKey nonce cipherText
-      liftIO $ decryptedText `shouldBe` plainText
+      decryptedText <- Box.decryptC combinedKey nonce cipherText
+      liftIO $ decryptedText `shouldBe` Just plainText
+
+  it "should decrypt encrypted data with a generated keypair" $
+    property $ \nonce plainText -> runTest $ do
+      KeyPair sk pk <- KeyPair.newKeyPairC
+      combinedKey <- CombinedKey.precomputeC sk pk
+      let cipherText = Box.encrypt combinedKey nonce plainText
+      decryptedText <- Box.decryptC combinedKey nonce cipherText
+      liftIO $ decryptedText `shouldBe` Just plainText
+
+  it "should decrypt encrypted data with a generated keypair" $
+    property $ \nonce plainText -> runTest $ do
+      KeyPair sk pk <- KeyPair.newKeyPairC
+      combinedKey <- CombinedKey.precomputeC sk pk
+      cipherText <- Box.encryptC combinedKey nonce plainText
+      let decryptedText = Box.decrypt combinedKey nonce cipherText
+      liftIO $ decryptedText `shouldBe` Just plainText
+
+  it "supports communication with asymmetric keys" $
+    property $ \nonce plainText -> runTest $ do
+      KeyPair sk1 pk1 <- KeyPair.newKeyPairC
+      KeyPair sk2 pk2 <- liftIO KeyPair.newKeyPair
+
+      key1 <- CombinedKey.precomputeC sk1 pk2
+      let key2 = CombinedKey.precompute sk2 pk1
+      liftIO $ key1 `shouldBe` key2
+
+      cipherText <- Box.encryptC key1 nonce plainText
+      let decryptedText = Box.decrypt key2 nonce cipherText
+      liftIO $ decryptedText `shouldBe` Just plainText
