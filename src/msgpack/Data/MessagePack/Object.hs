d9d91f54d03195d1d15176aca5ee390880af0083
Eduard Nicodei <eddnicodei@gmail.com>
2016-07-09 00:20:25 +0100

    Add text <-> binary conversion tool for MessagePack.
    This tool provides a way to translate to and from msgpack Object
    using standard input / output.
    
    It performs two kinds of conversions:
    1. plain text -> Data.MessagePack.Object -> msgpack binary
    2. msgpack binary -> Data.MessagePack.Object -> plain text
    
    It automatically detects which of the two it should perform.
    
    For more information, see comments in MessagePackParser.hs.

--- src/msgpack/Data/MessagePack/Object.hs
+++ src/msgpack/Data/MessagePack/Object.hs
@@ -49,7 +49,7 @@ data Object
   | ObjectExt    {-# UNPACK #-} !Word8 !S.ByteString
     -- ^ represents a tuple of an integer and a byte array where
     -- the integer represents type information and the byte array represents data.
-  deriving (Show, Eq, Ord, Typeable, Generic)
+  deriving (Read, Show, Eq, Ord, Typeable, Generic)
 
 instance NFData Object
 
