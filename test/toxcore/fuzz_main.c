8ca4da4a9ccf84cd1b16ca2bd82c9993d628669e
iphydf <iphydf@users.noreply.github.com>
2016-07-10 21:00:36 +0200

    Add support for afl-fuzz-fast.
    This gives about 10x speedup, allowing for much faster fuzzing cycles.

--- test/toxcore/fuzz_main.c
+++ test/toxcore/fuzz_main.c
@@ -3,9 +3,19 @@
 #include <unistd.h>
 
 
+// The number of afl iterations before terminating the process and starting a
+// new one.
+// See https://github.com/mcarpenter/afl/blob/master/llvm_mode/README.llvm.
+#define ITERATIONS 1000
+
+
 int
 main (int argc, char **argv)
 {
   struct settings cfg = { false, false };
-  return communicate (cfg, STDIN_FILENO, STDOUT_FILENO);
+#ifdef __AFL_LOOP
+  while (__AFL_LOOP (ITERATIONS))
+#endif
+    communicate (cfg, STDIN_FILENO, STDOUT_FILENO);
+  return 0;
 }
