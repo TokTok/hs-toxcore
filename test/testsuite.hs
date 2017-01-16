9ef38f73e5870e72757be116f8b411d5be8aba37
iphydf <iphydf@users.noreply.github.com>
2017-01-16 11:49:40 +0000

    Allow user to specify custom args to the testsuite.

--- test/testsuite.hs
+++ test/testsuite.hs
@@ -1,17 +1,17 @@
 module Main (main) where
 
 import           Control.Concurrent.Async (cancel, wait, withAsync)
-import           System.Environment       (withArgs)
+import           System.Environment       (getArgs, withArgs)
 
 import           Network.Tox.Testing      (serve)
 import qualified ToxTestSuite
 
 
 main :: IO ()
-main =
+main = do
+  args <- getArgs
+  let client = withArgs (args ++ ["--print-cpu-time", "--color"]) ToxTestSuite.main
   withAsync serve $ \server ->
     withAsync client $ \res -> do
       wait res
       cancel server
-  where
-    client = withArgs ["--print-cpu-time", "--color"] ToxTestSuite.main
