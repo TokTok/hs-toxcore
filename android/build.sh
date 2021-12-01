271efa49f278afe06f9febaffd2439283b4dc89e
Restyled.io <commits@restyled.io>
2021-12-01 12:09:39 +0000

    Restyled by shfmt

--- android/build.sh
+++ android/build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-export THIS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-. "$HOME/ghc-build/set-env-android.sh"; cd -
+export THIS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+. "$HOME/ghc-build/set-env-android.sh"
+cd -
 set -x
 
 "$NDK_TARGET-cabal" update
@@ -9,6 +10,6 @@ set -x
 "$THIS_DIR/user-scripts/install-libsodium.sh"
 "$THIS_DIR/user-scripts/cabal-prepare.sh" comonad-5.0.8 distributive-0.6.2.1 entropy-0.4.1.6 network-3.1.1.1
 
-(cd network-3.1.1.1 && patch -p1 < ../android/patches/hs-network-ffi.patch)
+(cd network-3.1.1.1 && patch -p1 <../android/patches/hs-network-ffi.patch)
 
 "$NDK_TARGET-cabal" new-install -j4 --with-happy="$(which happy)" --ghc-option="-fPIC"
